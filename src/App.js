020949077ca5f420352c2e1f661d64055edf4729
DaPickle <82292540+DaPickle@users.noreply.github.com>
2023-05-26 17:38:50 -0400

    Fix background not adjusting if stocks go off page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./index.css";
 function App() {
   return (
     // bg-[rgb(245,225,193)]
-    <div className="App bg-orange-100 w-full h-screen">
+    <div className="App bg-orange-100 w-full min-h-screen">
       <nav className="navbar bg-[rgb(59,59,76)] w-full">
         <div to="/" className="navbar-logo  font-bold text-white text-left text-6xl font-size-20 p-3 w-full">
           Portfolio Calculator
